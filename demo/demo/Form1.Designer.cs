0ddbff0b01be707a56d332b154883bf03e9e297e
Huyền Trân <119440149+huyentran1712@users.noreply.github.com>
2023-09-20 08:19:31 +0700

    Update Form1.Designer.cs
    update1

--- demo/demo/Form1.Designer.cs
+++ demo/demo/Form1.Designer.cs
@@ -35,17 +35,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(87, 58);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.Size = new System.Drawing.Size(130, 31);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Bấm vô đuy";
+            this.label1.Text = "Nhấp vào";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(90, 112);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(175, 121);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(108, 43);
             this.button1.TabIndex = 1;
             this.button1.Text = "Bùm";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,7 +57,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(356, 243);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
